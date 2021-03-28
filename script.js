b08604bf6c14d81d16d6aa5e146a6f6c4dc17716
DayaneAlmeida <78007041+DayaneAlmeida@users.noreply.github.com>
2021-03-28 00:11:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+var primeiroValor = parseInt(prompt("Digite o primeiro valor:"))
+var segundoValor = parseInt(prompt("Digite o segundo valor:"))
+
+var operacao = parseInt(prompt("Digite 1 para dividir, 2 para multiplicar, 3 para somar ou 4 para subtrair:"))
+
+if (operacao == 1){ //if= se //else=senao
+  var resultado = primeiroValor / segundoValor
+  document.write("<h2>"+ primeiroValor + " / " + segundoValor + " = " + resultado + "</h2>")
+} else if (operacao == 2){ //if= se //else=senao
+  var resultado = primeiroValor * segundoValor
+  document.write("<h2>"+ primeiroValor + " x " + segundoValor + " = " + resultado + "</h2>")
+} else if (operacao == 3){ //if= se //else=senao
+  var resultado = primeiroValor + segundoValor
+  document.write("<h2>"+ primeiroValor + " + " + segundoValor + " = " + resultado + "</h2>")
+} else if (operacao == 4){ //if= se //else=senao
+  var resultado = primeiroValor - segundoValor
+  document.write("<h2>"+ primeiroValor + " - " + segundoValor + " = " + resultado + "</h2>")
+} else {
+  document.write("<h2>Opção inválida!</h2>")
+}
